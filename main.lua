c74956a2dc20dcb4213a0e25ef833338cbbf6367
Gabriel Pérez Paredes <gabogabygpp16@gmail.com>
2024-02-20 23:51:58 -0400

    Agregar archivos del inge

--- main.lua
+++ main.lua
@@ -14,11 +14,11 @@ local composer = require "composer"
 
 -- event listeners for tab buttons:
 local function onFirstView( event )
-	composer.gotoScene( "view1" )
+    composer.gotoScene( "view1" )
 end
 
 local function onSecondView( event )
-	composer.gotoScene( "view2" )
+    composer.gotoScene( "view2" )
 end
 
 
@@ -26,14 +26,14 @@ end
 
 -- table to setup buttons
 local tabButtons = {
-	{ label="First", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onFirstView, selected=true },
-	{ label="Second", defaultFile="button2.png", overFile="button2-down.png", width = 32, height = 32, onPress=onSecondView },
+    { label="First", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onFirstView, selected=true },
+    { label="Second", defaultFile="button2.png", overFile="button2-down.png", width = 32, height = 32, onPress=onSecondView },
 }
 
 -- create the actual tabBar widget
 local tabBar = widget.newTabBar{
-	top = display.contentHeight - 50,	-- 50 is default height for tabBar widget
-	buttons = tabButtons
+    top = display.contentHeight - 50,	-- 50 is default height for tabBar widget
+    buttons = tabButtons
 }
 
-onFirstView()	-- invoke first tab button's onPress event manually
+composer.gotoScene( "splashScreen" )	-- invoke splashScreen scene
